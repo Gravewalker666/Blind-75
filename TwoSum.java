ac2fa3270fb9a3a2b215a172e9c1c6646a0ee2ee
Gravewalker666 <heshanandrews99@gmail.com>
2022-04-16 17:18:51 +0530

    Problem 1: Two sum

--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,19 @@
-public class TwoSum {
+import java.util.HashMap;
 
-    public static void main(String[] args) {
-        System.out.println("it works");
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numToIndex = new HashMap<>();
+        int[] output = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (numToIndex.containsKey(difference)) {
+                output[0] = i;
+                output[1] = numToIndex.get(difference);
+                break;
+            } else {
+                numToIndex.put(nums[i], i);
+            }
+        }
+        return output;
     }
 }
